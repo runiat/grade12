29024b0fea6d0ab83d3f7176f0fc3085cd8b7aa4
Mrs. Uniat <r87patel@gmail.com>
2016-01-12 09:54:01 -0500

    template for fibonacci recursice method for students

---
+++ a3recursion/ex1.py
@@ -0,0 +1,25 @@
+# this is an example of a recursive method
+
+def sum_num(num):
+    '''
+    This function will recursivly determine the sum of the digits in an integer number.
+    :param num:  a positive integer
+    :return: an integer that is the sum of the digits
+    '''
+    # base case. if there is only one digit return it.
+    if(num//10==0):
+        return num
+
+    # recursive case.
+    # If there is more than one digit, take off the last digit and call sum_num on a remaining number
+    else:
+        return num%10 + sum_num(num//10)
+
+
+def main():
+    test_numbers=[4,12,145,12435,8765903]
+    for i in range(5):
+         print("Test {0} Input {1}. Output: {2}".format(i,test_numbers[i],sum_num(test_numbers[i])))
+
+
+main()
---
+++ a3recursion/ex2.py
@@ -0,0 +1,23 @@
+# this is an example of the fibonacci sequence
+# fibonacci numbers: 1, 1, 2, 3, 5, 8, 13,...
+#         positions: F1,F2,F3,F4,F5,F6,F7,...
+
+
+def fibonacci(n):
+    '''
+    :param n: an integer. Fn The nth number in the fibonacci sequence.
+    :return: the value of the nth number
+    '''
+
+    # base case
+    pass
+
+
+    # recusrive case
+
+
+def main():
+    # test your program here
+    pass
+
+main()
