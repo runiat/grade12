d561d377a00fb43ae59ca701642af02daf61e3a2
Mrs. Uniat <r87patel@gmail.com>
2016-01-18 11:02:59 -0500

    exercise0 lists and dictionaries exercises template

---
+++ a4sorting/Exercise0ListsDict.py
@@ -0,0 +1,54 @@
+#  https://erlerobotics.gitbooks.io/erle-robotics-learning-python-gitbook-free/content/lists/exercises_list_and_dictionaries.html
+# Exercises Lists and Dictionaries
+# lists use [value,value,...], dictionaries use {key1:value,key2:value}
+
+# Exercise 1
+
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'],
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+
+
+#Exersise 2
+
+
+
+#Exercise 3
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+
+#Exercise 4
+
+lloyd = {
+  "name": "Lloyd",
+  "homework": [90.0,97.0,75.0,92.0],
+  "quizzes": [88.0,40.0,94.0],
+  "tests": [75.0,90.0]
+}
+alice = {
+  "name": "Alice",
+  "homework": [100.0, 92.0, 98.0, 100.0],
+  "quizzes": [82.0, 83.0, 91.0],
+  "tests": [89.0, 97.0]
+}
+tyler = {
+  "name": "Tyler",
+  "homework": [0.0, 87.0, 75.0, 22.0],
+  "quizzes": [0.0, 75.0, 78.0],
+  "tests": [100.0, 100.0]
+}
